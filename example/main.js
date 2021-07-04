fce9090d33dc4560e8becc12699e62e2735d34b0
liuyufeng <jjxliu306@163.com>
2021-07-04 17:45:12 +0800

    1、对element vue 等组件进行cdn引用。2、发布2.0.13版本，输出弹性容器组件。3、优化部分代码

--- example/main.js
+++ example/main.js
@@ -2,8 +2,8 @@
 import Vue from 'vue'
 import App from './App'  
 // 导入element-ui    
-import ElementUI from 'element-ui';
-import 'element-ui/lib/theme-chalk/index.css';
+//import ElementUI from 'element-ui';
+//import 'element-ui/lib/theme-chalk/index.css';
  
  // 导入组件库
 import VueFormDesign from '../packages/index'
@@ -11,7 +11,7 @@ import '../packages/vue-drag-formdesign/src/form-design.css';
 // 注册组件库
 Vue.use(VueFormDesign)
 
-Vue.use(ElementUI);
+//Vue.use(ElementUI);
  
 
 
--- vue.config.js
+++ vue.config.js
@@ -1,27 +1,59 @@
+ 
 module.exports = {
   // 修改 src 目录 为 examples 目录
   pages: {
     index: {
       entry: 'example/main.js',
       template: 'public/index.html',
-      filename: 'index.html'
+      filename: 'index.html',
+      chunks: ['chunk-vendors', 'chunk-common', 'index'],
+      cdn: {
+        css: [
+          'https://cdn.jsdelivr.net/npm/element-ui@2.13.2/lib/theme-chalk/index.css'
+        ],
+        js: [
+          "https://cdn.jsdelivr.net/npm/vue@2.6.11/dist/vue.min.js",
+          "https://cdn.jsdelivr.net/npm/vue-router@3.0.3/dist/vue-router.min.js",
+          "https://cdn.jsdelivr.net/npm/vuex@3.1.3/dist/vuex.min.js",
+          "https://cdn.jsdelivr.net/npm/element-ui@2.13.2/lib/index.js",
+          "https://cdn.jsdelivr.net/npm/axios@0.18.0/dist/axios.min.js",
+          "https://cdn.jsdelivr.net/npm/moment@2.24.0/moment.min.js" 
+        ]
+ 
+      }, 
     }
   },
+  configureWebpack: {
+    // 打包忽略文件
+    externals: {
+      "element-ui": "ELEMENT",
+      vue: "Vue",
+      "vue-router": "VueRouter",
+      vuex: "Vuex",
+      axios: "axios",
+      moment: "moment" 
+    }
+  }, 
   productionSourceMap: false,
   // 强制内联CSS
-  css: { extract: true },
+  css: { extract: true }, 
   // 扩展 webpack 配置，使 packages 加入编译
-  chainWebpack: config => {
+  chainWebpack: config => { 
+    //config.plugin('webpack-bundle-analyzer')
+    //    .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
     config.module
       .rule('js')
       .include
         .add('/packages')
         .end()
+
       .use('babel')
         .loader('babel-loader')
         .tap(options => {
           // 修改它的选项...
           return options
         })
-  }
+       
+  } 
+ 
 }
