00b0a4367fac8f2a4729a6952f3bcbd709052726
lyf <jjxliu306@163.com>
2023-03-04 22:43:43 +0800

    text标签的动态必选

--- packages/form-design/items/decorate/text/index.vue
+++ packages/form-design/items/decorate/text/index.vue
@@ -1,7 +1,7 @@
 <template>  
-	<div class="form-label"  :style="{ textAlign: record.options.textAlign }" > 
+	<div class="ng-form-label"  :style="{ textAlign: record.options.textAlign }" > 
       <label
-        :class="{ 'is-required': record.options.showRequiredMark || showRequiredMark }"
+        :class="{ 'is-required': showRequiredMark }"
         v-text="record.label"
       ></label>
   </div>
@@ -14,8 +14,11 @@ export default {
 	computed: {
 		showRequiredMark(){
 	      //##############
+	      if(!this.record.options.showRequiredMark) return false
+	      
+
 	      const fstr = this.record.options.showRequiredMarkScript
-	      if(!fstr){
+	      if(!fstr || !fstr.trim()){
 	        return false
 	      }
 
@@ -26,3 +29,12 @@ export default {
 	}
 }
 </script>
+<style lang="scss">
+.ng-form-label {
+	.is-required:before {
+	  content: "*";
+	  color: #F56C6C;
+	  margin-right: 4px;
+	}
+} 
+</style>
