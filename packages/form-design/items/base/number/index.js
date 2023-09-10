db3ce688a92309c13a0f03407f01470624899e54
lyf <jjxliu306@163.com>
2023-09-10 10:25:10 +0800

    数字输入快精度表示小数点后几位，只能为整数

--- packages/form-design/items/base/number/index.js
+++ packages/form-design/items/base/number/index.js
@@ -87,6 +87,8 @@ obj.options = {
                     label: $t('ngform.item.number.precision') ,//'精度', 
                     prop: 'precision',
                     type: 'number',
+                    precision: 0,
+                    step: 1,
                     default: 0 ,
                     span: 24,
                 },
