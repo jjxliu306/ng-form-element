63d0882db872ee5442d47b1ff2df7a895a7d3d79
Nottyjay <891084418@qq.com>
2023-11-06 16:50:26 +0800

    预览页面注入逻辑变更

--- example/main.js
+++ example/main.js
@@ -1,13 +1,15 @@
- 
+
 import Vue from 'vue'
-import App from './App'  
-   
+import App from './App'
+
+import ElementUI from 'element-ui'
+import 'element-ui/lib/theme-chalk/index.css';
 import deepmerge from 'deepmerge';
-  
+
 // 自定义组件
-import NgComponents from './components/index.js' 
+import NgComponents from './components/index.js'
  // 导入组件库
-import FormDesign from '../packages/index.js' 
+import FormDesign from '../packages/index.js'
 // ng-form组件国际化资源
 import zh from '../packages/locale/lang/zh_CN'
 import en from '../packages/locale/lang/en'
@@ -15,17 +17,16 @@ import en from '../packages/locale/lang/en'
 // 本地国际化资源
 import ngEn from './locale/en.js'
 import ngZh from './locale/zh_CN.js'
- 
+
 // 将本地的国际化资源和组件内资源合并
 const mergeZh =  deepmerge(zh, ngZh, { clone: true })
- 
-// 注册组件库
-Vue.use(FormDesign , {locale: mergeZh , components: NgComponents})
-  
 
+// 注册组件库
+Vue.use(ElementUI)
+Vue.use(FormDesign , {locale: mergeZh , components: NgComponents})
 Vue.config.productionTip = false
 
-new Vue({ 
+new Vue({
  // router,
   render: h => h(App),
 }).$mount('#app')
--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,31 @@
- 
+
+const IS_PROD = process.env.NODE_ENV === 'production'
+
+const npmConfigArgv = (process.env.npm_config_argv === undefined) ? null : JSON.parse(process.env.npm_config_argv)
+/*
+console.log('npm config: ', npmConfigArgv)
+const procArgv = process.argv
+console.log('npm config: ', procArgv)
+*/
+let buildProdFlag = false
+if (!!npmConfigArgv) {
+  npmConfigArgv.original.forEach(cItem => {
+    if (cItem === 'build') {
+      buildProdFlag = true
+    }
+  })
+}
+
+const mvdir = require('mvdir');
+if (IS_PROD && buildProdFlag) {
+  mvdir('index_template/index_prod.html', 'public/index.html', { copy: true });
+} else {
+  mvdir('index_template/index_dev.html', 'public/index.html', { copy: true });
+}
+
 module.exports = {
+  /* 开启vue运行时模板编译功能！！ */
+  runtimeCompiler: true,
   // 修改 src 目录 为 examples 目录
   pages: {
     index: {
@@ -9,16 +35,16 @@ module.exports = {
       chunks: ['chunk-vendors', 'chunk-common', 'index'],
       cdn: {
         css: [
-          '/cdn/element-ui-2.5.13.css' 
+          '/cdn/element-ui-2.5.13.css'
         ],
         js: [
           "/cdn/vue.min.js",
           //"/cdn/vue-router.min.js",
           "/cdn/vuex.min.js",
-          "/cdn/element-ui-2.5.13.js" 
+          "/cdn/element-ui-2.5.13.js"
         ]
- 
-      }, 
+
+      },
     }
   },
   configureWebpack:  config => {
@@ -27,25 +53,18 @@ module.exports = {
       config.externals = {
         "element-ui": "ELEMENT",
         // 编译 build的时候放开
-        // vue: "Vue",
-        //"vue-router": "VueRouter",
-        vuex: "Vuex" 
-      }
-    }else {
-      config.externals = {
-        "element-ui": "ELEMENT",
         vue: "Vue",
         //"vue-router": "VueRouter",
-        vuex: "Vuex" 
+        vuex: "Vuex"
       }
     }
-    
-  }, 
+
+  },
   productionSourceMap: false,
   // 强制内联CSS
-  css: { extract: true }, 
+  css: { extract: true },
   // 扩展 webpack 配置，使 packages 加入编译
-  chainWebpack: config => { 
+  chainWebpack: config => {
     //config.plugin('webpack-bundle-analyzer')
     //    .use(require('webpack-bundle-analyzer').BundleAnalyzerPlugin)
     if(process.env.NODE_ENV === 'production'){
@@ -70,7 +89,7 @@ module.exports = {
           // 修改它的选项...
           return options
         })
-       
-  } 
- 
+
+  }
+
 }
